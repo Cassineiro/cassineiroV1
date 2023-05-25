a77445352112520092644f197a174a6594cd34fc
Pedroo-Nietoo <pedronieto.2005@gmail.com>
2023-05-24 22:04:22 -0300

    Só tirei o null do alert

--- login/loginFirebase.js
+++ login/loginFirebase.js
@@ -1,31 +1,31 @@
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBWYed6XE2-YW_Aj2udOY7UIAW_1Y3fegY",
-    authDomain: "cassineiro-70605.firebaseapp.com",
-    projectId: "cassineiro-70605",
-    storageBucket: "cassineiro-70605.appspot.com",
-    messagingSenderId: "987748691719",
-    appId: "1:987748691719:web:37cd6b2b4ba28c74261fa2",
-    measurementId: "G-SXSLJK930J"
+  apiKey: "AIzaSyBWYed6XE2-YW_Aj2udOY7UIAW_1Y3fegY",
+  authDomain: "cassineiro-70605.firebaseapp.com",
+  projectId: "cassineiro-70605",
+  storageBucket: "cassineiro-70605.appspot.com",
+  messagingSenderId: "987748691719",
+  appId: "1:987748691719:web:37cd6b2b4ba28c74261fa2",
+  measurementId: "G-SXSLJK930J"
 };
 
 firebase.initializeApp(firebaseConfig);
 
-function login(){
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("senha").value;
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("senha").value;
 
-    firebase.auth().signInWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-            const user = userCredential.user;
-            const welcomeMessage = `Bem-vindo(a), ${user.displayName}!`;
-            alert(welcomeMessage);
-            window.location.href = "../home/home.html";
-        })
-        .catch((error) => {
-            console.log('Erro ao realizar o login:', error.code, error.message);
-            alert("Falha ao realizar o login, tente novamente.");
-        });
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      const welcomeMessage = `Bem-vindo(a) de volta!`;
+      alert(welcomeMessage);
+      window.location.href = "../home/home.html";
+    })
+    .catch((error) => {
+      console.log('Erro ao realizar o login:', error.code, error.message);
+      alert("Falha ao realizar o login, tente novamente.");
+    });
 }
 //login com o Google
 const googleSignupButton = document.getElementById('googleSignupButton');
